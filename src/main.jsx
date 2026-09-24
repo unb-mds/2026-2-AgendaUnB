a288671917b4085937b96587e9e2a36bdade0c4c
Thomas <thomasgusto12@gmail.com>
2026-09-24 18:32:34 -0300

    feat: Implementação do Google OAuth e conexão com o front-end.

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import SecondPage from './pages/SecondPage.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 import './styles/global.css'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/segunda-pagina" element={<SecondPage />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/segunda-pagina" element={<SecondPage />} />
+      </Routes>
+    </AuthProvider>
   </BrowserRouter>
 )
